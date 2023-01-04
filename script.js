42545bd7fc7db94bb45e5df65c426dd8db30c217
Joshua Rosado Olivencia <joshua06rosado@gmail.com>
2023-01-03 21:52:06 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+// var counter = 0;
+function jump () {
+    if(character.classList !=  "animate")
+    character.classList.add('animate');
+    setTimeout(function(){
+        character.classList.remove("animate")
+    }, 500);
+}
+
+var checkDead = setInterval(function(){
+    var characterTop = 
+    parseInt(
+    window.getComputedStyle(character).getPropertyValue("top"));
+    // counter++;
+
+    var blockLeft = 
+    parseInt(
+    window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft < 280 && blockLeft >0 && characterTop >=625 ){
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("You lost!");
+
+    }
+},10);
